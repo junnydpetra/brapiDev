d99df289aaf278169d11853bb22bde9e2fdcdbdd
junnydpetra <junnycmenezes@gmail.com>
2023-01-02 09:15:19 -0300

    centralização de cards

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,72 +4,66 @@
 
 @section('content')
 
-        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom border-dark">
             <h2>Ações Em Alta</h2>
-          </div>
+        </div>
 
-          <div class="table-responsive">
+        <div class="container">
+            <div class="row" style="margin-left: 35px;">
+                <div class="col-md" style="margin: 0 auto; width: 50%;">
 
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg"></div>
-                    <div class="col-lg">
-                        <table class="table table-success text-center table-bordered">
+                    @foreach ($apiArray as $api)
 
-                            @foreach ($apiArray as $api)
-                                <tr>
-                                    <td class="card mb-2">
-                                        <img src="{{ $api->logo }}" class="img-thumbnail mb-1">
-                                        <button type="button" href="{{ route('stockInfoModal', $api->stock) }}" class="btn btn-success btn-sm btn-modal" data-bs-toggle="modal" data-bs-target="#myModal">{{ $api->stock }}</button>
-                                    </td>
-                                </tr>
+                                <div class="d-inline-flex p-2 card d-flex justify-content-center border border-dark mb-1 bg-light"
+                                style="width: 200px; margin: 25px 15px;">
+                                    <img src="{{ $api->logo }}" class="card-img-top img-fluid mb-1 border-bottom border-dark">
+                                    <div class="card-body text-center">
+                                        <button type="button" href="{{ route('stockInfoModal', $api->stock) }}" class="btn btn-outline-warning border border-dark btn-sm btn-modal text-dark" data-bs-toggle="modal" data-bs-target="#myModal">{{ $api->stock }}</button>
+                                    </div>
+                                </div>
 
-                                {{-- <tr><img src="" alt=""></tr> --}}
+                            <!-- Modal -->
+                            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                                <div class="modal-dialog modal-lg" role="document">
+                                    <div class="modal-content bg-light p-2 text-dark bg-opacity-25 bg-gradient">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title" id="myModalLabel">Últimos Relatórios</h4>
+                                        </div>
 
-                                    <!-- Modal -->
-                                    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                                        <div class="modal-dialog modal-lg" role="document">
-                                            <div class="modal-content bg-success bg-success p-2 text-white bg-opacity-25 bg-gradient">
-                                                <div class="modal-header">
-                                                    <h4 class="modal-title" id="myModalLabel">Últimos Relatórios</h4>
-                                                </div>
+                                        <div class="modal-body" id="modal-body-stock">
 
-                                                <div class="modal-body" id="modal-body-stock">
+                                        </div>
 
-                                                </div>
-
-                                                <div class="modal-footer">
-                                                    <a><button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button></a>
-                                                    <button type="button" class="btn btn-primary">Save changes</button>
-                                                </div>
-                                            </div>
+                                        <div class="modal-footer">
+                                            <a><button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button></a>
+                                            <button type="button" class="btn btn-primary">Save changes</button>
                                         </div>
                                     </div>
+                                </div>
+                            </div>
 
+                    @endforeach
 
-                            @endforeach
-                        </table>
-                    </div>
-                    <div class="col-lg"></div>
                 </div>
             </div>
-
         </div>
-    </main>
 
-    @endsection
+    </div>
+</main>
 
-    @push('scripts')
-        <script>
-            $(function(){
-                $('.btn-modal').on('click', function() {
-                    let url = $(this).attr('href');
+@endsection
 
-                    $.get(url, function(data) {
-                        $('#modal-body-stock').html(data)
-                    })
+@push('scripts')
+    <script>
+        $(function(){
+            $('.btn-modal').on('click', function() {
+                let url = $(this).attr('href');
+
+                $.get(url, function(data) {
+                    $('#modal-body-stock').html(data)
                 })
             })
-        </script>
-    @endpush
+        })
+    </script>
+@endpush
--- resources/views/modal-stock.blade.php
+++ resources/views/modal-stock.blade.php
@@ -1,35 +1,4 @@
-{{-- <table class="table border border-dark table-success text-center">
-    <thead>
-        <th>AÇÃO</th>
-        <th>SHORT NAME</th>
-        <th>EMPRESA</th>
-        <th>VALOR REGULAR DE MERCADO</th>
-        <th>VALOR MAIS ALTO</th>
-        <th>VALOR MAIS BAIXO</th>
-    </thead>
-
-    <tbody>
-        <td>{{ $stock->symbol }}</td>
-        <td>{{ $stock->shortName }}</td>
-        <td>{{ $stock->longName }}</td>
-        <td>{{ $stock->regularMarketPrice }}</td>
-        <td>{{ $stock->regularMarketDayHigh }}</td>
-        <td>{{ $stock->regularMarketDayRange }}</td>
-    </tbody>
-
-</table> --}}
-
-{{--
-    {{$stock->symbol}}
-    {{$stock->shortName}}
-    {{$stock->longName}}
-    {{$stock->regularMarketPrice}}
-    {{$stock->regularMarketDayHigh}}
-    {{$stock->regularMarketDayLow}} --}}
-
-
-
-<div class="modal-body border border-dark bg-danger bg-gradient" >
+<div class="bg-light bg-gradient" >
     <b>AÇÃO: </b>{{$stock->symbol}}<br>
     <b>SHORT NAME: </b>{{$stock->shortName}}<br>
     <b>EMPRESA: </b>{{$stock->longName}}<br>
